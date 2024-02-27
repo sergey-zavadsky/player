601759ff9e1e76dd57e9bde9a7ceee108fa7c4cb
Siarhei Zavadski <siarhei.zavadski@capital.com>
2024-02-27 20:09:14 +0100

    trying OG tags

--- src/components/player/Song/Song.tsx
+++ src/components/player/Song/Song.tsx
@@ -3,12 +3,19 @@ import Player from '../../player/css/Player';
 import { useRecoilState } from 'recoil';
 import { currentSongAtom } from '../../../atoms/songState';
 import { ChillHopTrack } from '../../../util/index';
+import { Helmet } from 'react-helmet';
 
 const Song = ({ songs }: { songs: ChillHopTrack[] }) => {
 	const [isCurrentIndex] = useRecoilState(currentSongAtom);
 	const current = songs[isCurrentIndex];
 	return (
 		<>
+			<Helmet>
+				<title>Siarhei Zavadski react player</title>
+				<meta property="og:title" content={current.artist} />
+				<meta property="og:description" content={current.name} />
+				<meta property="og:image" content={current.cover} />
+			</Helmet>
 			<div className={styles['song-container']}>
 				<img src={current.cover} alt={current.artist} />
 				<h2>{current.name}</h2>
